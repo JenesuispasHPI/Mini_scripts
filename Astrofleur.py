adf72894444041ce0ab109edcbfe479ee08718ee
T <141818631+JenesuispasHPI@users.noreply.github.com>
2023-08-11 22:47:43 +0200

    Create Astrofleur.py
    Deviner la fleur correspondant au signe astrologique.

---
+++ Astrofleur.py
@@ -0,0 +1,27 @@
+def find_flower(sign):
+    flowers = {
+        "bélier": "tulipe",
+        "taureau": "rose",
+        "gémeaux": "lavande",
+        "cancer": "lys",
+        "lion": "tournesol",
+        "vierge": "jacinthe",
+        "balance": "orchidée",
+        "scorpion": "chrysanthème",
+        "sagittaire": "pivoine",
+        "capricorne": "edelweiss",
+        "verseau": "bleuet",
+        "poissons": "jonquille"
+    }
+    
+    sign = sign.lower()
+    
+    if sign in flowers:
+        return flowers[sign]
+    else:
+        return "Signe astrologique non valide."
+
+sign_input = input("Entrez votre signe astrologique : ")
+flower = find_flower(sign_input)
+
+print(f"Votre signe astrologique est {sign_input.capitalize()} et la fleur qui vous correspond est la {flower}.")
