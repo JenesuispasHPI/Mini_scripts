32e78bfd7b4f84522b5611c8c59883388a278acb
T <141818631+JenesuispasHPI@users.noreply.github.com>
2023-08-11 23:14:14 +0200

    Create CouleursFleurs.py
    entrer nom de fleur 🌷 et trouver la couleur

---
+++ CouleursFleurs.py
@@ -0,0 +1,61 @@
+# Dictionnaire des fleurs et de leurs couleurs
+fleurs_et_couleurs = {
+    "rose": "rouge",
+    "tulipe": "jaune",
+    "pivoine": "rose",
+    "marguerite": "blanche",
+    "lavande" : "violet",
+    "lys" : "blanc",
+    "orchidée": "violet",
+    "coquelicot": "rouge",
+	  "jonquille": "jaune",
+	  "Bleuet": "Bleu",
+	  "Camélia": "Rouge",
+	  "Muguet" : "Blanc",
+    "Iris" : "Violet",
+	  "Nénuphar": "Blanc",
+	  "Glaïeul": "Multicolore",
+	  "Lilas": "Violet",
+	  "Hortensia": "Rose",
+	  "Jasmin" : "Blanc",
+	  "Renoncule": "Multicolore",
+	  "Œillet": "Rouge",
+	  "Anémone": "Rouge",
+	  "Chrysanthème": "Jaune",
+	  "Primevère": "Jaune",
+    "Souci": "Orange",
+	  "Amaryllis": "Rouge",
+	  "Edelweiss": "Blanc",
+	  "Fuchsia": "Rose",
+	  "Gentiane": "Bleu",
+    "Bégonia": "Multicolore",
+	  "Églantine": "Rose",
+
+}
+
+def trouver_couleur_fleur(fleur):
+    if fleur in fleurs_et_couleurs:
+        return fleurs_et_couleurs[fleur]
+    else:
+        return "Fleur non trouvée"
+
+def trouver_fleurs_par_couleur(couleur):
+    fleurs = [fleur for fleur, col in fleurs_et_couleurs.items() if col == couleur]
+    if fleurs:
+        return fleurs
+    else:
+        return "Couleur non trouvée"
+
+# Demander au client le nom de la fleur ou la couleur
+entree = input("Entrez le nom d'une fleur ou une couleur : ")
+
+# Vérifier si l'entrée correspond à une fleur ou une couleur
+if entree in fleurs_et_couleurs:
+    couleur_correspondante = trouver_couleur_fleur(entree)
+    print(f"La fleur {entree} est de couleur {couleur_correspondante}")
+else:
+    fleurs_correspondantes = trouver_fleurs_par_couleur(entree)
+    if isinstance(fleurs_correspondantes, list):
+        print(f"Les fleurs de couleur {entree} sont : {', '.join(fleurs_correspondantes)}")
+    else:
+        print(fleurs_correspondantes)
